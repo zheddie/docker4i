33a9a01882890063dac55a3081b0ab7e769fcc73
GavinZhang <zheddie@163.com>
2022-06-14 22:32:03 +0800

    support build env for gcc10 and python39

--- setupbld39env/localsetup.sh
+++ setupbld39env/localsetup.sh
@@ -3,10 +3,12 @@ export PATH=/QOpenSys/pkgs/bin/:$PATH
 yum update -y
 yum install -y python39 python39-devel python39-pip
 python3.9 -m pip install --upgrade pip
-yum install -y gcc gcc-cplusplus gcc-aix gcc-cpp-aix gcc-cplusplus-aix libstdcplusplus-devel 
+yum install -y gcc10 gcc10-cplusplus gcc10-cpp gcc10-cplusplus gcc10-gfortran libstdcplusplus6-10-devel 
 release=`uname -vr`
 if [ "*$release*" == "*5 7*" ]; then
-mv  /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/6.3.0/include-fixed-7.1/sys/types.h  /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/6.3.0/include-fixed-7.1/sys/types.h.nouse
+	echo "Fixing for gcc10"
+	#for gcc10, we may not needed it anymore.
+	#mv  /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/6.3.0/include-fixed-7.1/sys/types.h  /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/6.3.0/include-fixed-7.1/sys/types.h.nouse
 fi
 yum install -y vim git wget curl gzip
 yum install -y patch-gnu m4-gnu grep-gnu sed-gnu tar-gnu
