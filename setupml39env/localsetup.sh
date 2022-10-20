d3d02272bf23dd3d2974e1342e6533f6074ded0a
GavinZhang <zhanggan@cn.ibm.com>
2022-10-20 16:28:30 +0800

    update setupml39env

--- setupml39env/localsetup.sh
+++ setupml39env/localsetup.sh
@@ -1,7 +1,8 @@
 #! /QOpenSys/pkgs/bin/bash
 export PATH=/QOpenSys/pkgs/bin/:~/gcc10bin:$PATH
+export OBJECT_MODE=64
 yum update -y
-yum install -y python3.9python39-devel python39-pip
+yum install -y python3.9 python39-devel python39-pip
 python3.9 -m pip install --upgrade pip
 yum install -y python39-ibm_db
 #yum install -y gcc gcc-cplusplus gcc-aix gcc-cpp-aix gcc-cplusplus-aix libstdcplusplus-devel 
@@ -27,6 +28,7 @@ if [ "*$release*" == "*5 7*" ]; then
 	fi
 fi
 yum install -y libcblas3     # try to avoid the dependency on libopenblas0 by python39-scipy.
+yum install -y libgfortran5  #used by lots of ML package.
 yum install -y python39-scipy #(1.1.0)   # python39-numpy (1.15.4)
 yum install -y python39-scikit-learn #(0.19.1)
 yum install -y tcl tk pkg-config python39-tkinter python39-six #six(1.10.0)
@@ -39,6 +41,7 @@ python3.9 -m pip install python-dateutil #required by pandas.
 yum install -y python39-psutil python39-pandas
 yum install -y libffi-devel       #Required by jupyter.
 yum install -y libjpeg-turbo-devel #Required by Pillow
+yum install -y qhull-devel    #required by matplotlib
 #yum install -y python39-pyzmq     # replaced by latest version of pyzmq
 python3.9 -m pip install pyzmq
 python3.9 -m pip install pybind11
@@ -47,6 +50,7 @@ python3.9 -m pip install Pillow==8.4.0
 python3.9 -m pip install jupyter==1.0.0              #Required six>=1.11.0
 python3.9 -m pip install jupyterlab==3.2.9
 python3.9 -m pip install cython==0.29.28
-#python3.9 -m pip install matplotlib==3.3.4
-#python3.9 -m pip install pandas==1.1.5
-#python3.9 -m pip install seaborn==0.11.2
+python3.9 -m pip install cppy kiwisolver    #Required by matplotlib 
+python3.9 -m pip install contourpy
+python3.9 -m pip install matplotlib
+#python3.9 -m pip install seaborn
