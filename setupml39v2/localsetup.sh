7d4a738d24fc899f105c16ed1e73c4d334a1e9a9
GavinZhang <zhanggan@cn.ibm.com>
2022-11-20 10:17:11 +0800

    trying matplotlib

--- setupml39v2/localsetup.sh
+++ setupml39v2/localsetup.sh
@@ -28,16 +28,18 @@ if [ "*$release*" == "*5 7*" ]; then
 		mv  /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/10/include-fixed/sys/types.h /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/10/include-fixed/sys/types.h.nouse
 	fi
 fi
-yum install -y pkg-config openblas-devel libzmq5
+yum install -y pkg-config openblas-devel 
+yum install -y libzmq5 libzmq-devel #for pyzmq
 
 yum install -y freetype-devel libjpeg-turbo-devel zlib-devel #for pillow
 yum install -y python39-psutil libxml2-devel libxslt-devel libffi-devel #for Jupyter
-yum install -y qhull-devel #for matplotlib
+
 #Prepare
 python3.9 -m pip install pybind11 cython
 python3.9 -m pip install --upgrade six
 #numpy
 python3.9 -m pip install numpy   #could installed, but need the PR to make the distutils works for scipy.
+
 #scipy
 # python3.9 -m pip install ply gast beniget pythran
 # python3.9 -m pip install ninja #This is required by pip3:ninja
@@ -46,34 +48,30 @@ python3.9 -m pip install numpy   #could installed, but need the PR to make the d
 # #
 python3.9 -m pip install scipy #It would also trying to install ninja as pip package. Also need a PR to make "func_data" changed.
 # #scikit-learn
-# #python3.9 -m pip install threadpoolctl joblib
+python3.9 -m pip install threadpoolctl joblib
+# waiting for the oldest_support_numpy project update.
 # python3.9 -m pip install scikit-learn  #Currently, need 
 
 # #Pandas
 # #python3.9 -m pip install python-dateutil pytz  #prereq by pandas.
+# could be installed successfully, but it's too lang time to install, temporarily remove it.
 # python3.9 -m pip install pandas
 # #Pillow
-# python3.9 -m pip install Pillow
+python3.9 -m pip install Pillow
 
 
 
 # #Jupyter
-# python3.9 -m pip install pyzmq
-# python3.9 -m pip install jupyter jupyterlab
-
-# #matplotlib
-# pip3 install matplotlib
-
+python3.9 -m pip install pyzmq
+python3.9 -m pip install jupyter jupyterlab
 
 #Matplotlib
-#yum install qhull #for matplotlib.
-#python3.9 -m pip install cppy kiwisolver    #Required by matplotlib 
-#python3.9 -m pip install contourpy
-#python3.9 -m pip install matplotlib      #Waiting on the available of qhull.
+# yum install -y qhull-devel #for matplotlib
+# # python3.9 -m pip install cppy kiwisolver    #Required by matplotlib 
+# # python3.9 -m pip install contourpy
+#python3.9 -m pip install matplotlib
 #python3.9 -m pip install seaborn
 
 #NO USE as for now.
 #yum install -y tcl tk python39-tkinter  #for matplotlib, may not need anymore?
 #yum install -y libpng-devel libpng16
-#yum install -y python39-dateutil python39-pytz  python39-six  #required by pandas. No need any more.
-
