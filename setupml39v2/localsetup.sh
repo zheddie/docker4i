3791ecc8b6ff4ea726f6b707789ca8c7932dfe3a
GavinZhang <zhanggan@cn.ibm.com>
2022-11-21 15:47:22 +0800

    latest base;fix RPM repo issue;adding more packages in ml39v2.

--- setupml39v2/localsetup.sh
+++ setupml39v2/localsetup.sh
@@ -21,13 +21,12 @@ ln -s /QOpenSys/pkgs/bin/g++-10 ~/gcc10bin/c++
 ln -s /QOpenSys/pkgs/bin/gfortran-10 ~/gcc10bin/gfortran
 release=`uname -vr`
 if [ "*$release*" == "*5 7*" ]; then
-	#openblas/0.3.19-1 on i 7.3 ,7.4 and 7.5would cause some error.
-	#yum install -y openblas openblas-devel
 	#for gcc10, we may not need it.
 	if [ -f "/QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/10/include-fixed/sys/types.h" ]; then
 		mv  /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/10/include-fixed/sys/types.h /QOpenSys/pkgs/lib/gcc/powerpc-ibm-aix6.1.0.0/10/include-fixed/sys/types.h.nouse
 	fi
 fi
+# yum install -y cmake #for scipy, ninja
 yum install -y pkg-config openblas-devel 
 yum install -y libzmq5 libzmq-devel #for pyzmq
 
@@ -44,34 +43,26 @@ python3.9 -m pip install numpy   #could installed, but need the PR to make the d
 # python3.9 -m pip install ply gast beniget pythran
 # python3.9 -m pip install ninja #This is required by pip3:ninja
 # yum install ninja-build  #This is required by pip3:ninja, while pip3:ninja would change the bin/ninja to python scirpt.
-
-# #
 python3.9 -m pip install scipy #It would also trying to install ninja as pip package. Also need a PR to make "func_data" changed.
+
 # #scikit-learn
 python3.9 -m pip install threadpoolctl joblib
 # waiting for the oldest_support_numpy project update.
 # python3.9 -m pip install scikit-learn  #Currently, need 
 
-# #Pandas
-# #python3.9 -m pip install python-dateutil pytz  #prereq by pandas.
-# could be installed successfully, but it's too lang time to install, temporarily remove it.
-# python3.9 -m pip install pandas
 # #Pillow
 python3.9 -m pip install Pillow
-
-
 
 # #Jupyter
 python3.9 -m pip install pyzmq
 python3.9 -m pip install jupyter jupyterlab
 
 #Matplotlib
-# yum install -y qhull-devel #for matplotlib
-# # python3.9 -m pip install cppy kiwisolver    #Required by matplotlib 
-# # python3.9 -m pip install contourpy
-#python3.9 -m pip install matplotlib
-#python3.9 -m pip install seaborn
+yum install -y qhull-devel #for matplotlib
+# # python3.9 -m pip install cppy kiwisolver  contourpy   #Required by matplotlib 
+python3.9 -m pip install matplotlib
+#This will auto install pandas very quickly. 
+python3.9 -m pip install seaborn
 
 #NO USE as for now.
-#yum install -y tcl tk python39-tkinter  #for matplotlib, may not need anymore?
 #yum install -y libpng-devel libpng16
--- setuprpmenv/localsetup.sh
+++ setuprpmenv/localsetup.sh
@@ -1,3 +1,7 @@
 #! /QOpenSys/usr/bin/bsh
 rm -rf /qopensys/pkgs /qopensys/var/lib/rpm /qopensys/var/lib/yum /qopensys/cache/yum /qopensys/etc/yum/repos.d /tmp/bootstrap.log
 system "QSH CMD('touch -C 819 /tmp/bootstrap.log; /QOpenSys/usr/bin/ksh /tmp/bootstrap.sh > /tmp/bootstrap.log 2>&1')"
+#as a work around of ibmi-base repo issue.
+/QOpenSys/pkgs/bin/yum update -y
+rm /qopensys/etc/yum/repos.d/ibmi-base.repo
+/QOpenSys/pkgs/bin/yum update -y
